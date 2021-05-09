deca6b79250752d786076b6c890f40df7ade6117
benmp2 <benmp2@github.com>
2021-05-09 13:34:00 +0200

    Moving chart generation to new file

---
+++ charts.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import plotly.graph_objs as go
+
+def generate_chart(df: pd.DataFrame, feature_name: str) -> go.Figure:
+
+    fig = go.Figure()
+    fig.add_scatter(x=df.index, y=df[feature_name], name=feature_name, showlegend=True)
+    fig.update_layout(title=f"Acceleration's {feature_name} feature chart")
+    return fig
+
+
+def generate_chart_with_rangeselector(df: pd.DataFrame, feature_name: str) -> go.Figure:
+
+    # Create figure
+    fig = go.Figure()
+
+    fig.add_scatter(x=df.index, y=df[feature_name], name=feature_name)
+
+    # Set title
+    fig.update_layout(title_text="Select the calibration period:")
+
+    # Add range slider
+    fig.update_layout(
+        xaxis=dict(
+            rangeselector=dict(
+                buttons=list(
+                    [
+                        dict(step="all"),
+                    ]
+                )
+            ),
+            rangeslider=dict(visible=True),
+            type="date",
+        )
+    )
+    return fig
