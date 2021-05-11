c5e2bf0c6e492fe558d4eb59b348b438b0515308
benmp2 <benjamin.powell@harisdigital.com>
2021-05-11 14:18:46 +0200

    removing global variable for dcc.Store

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ import charts
 import plotly.graph_objs as go
 from plotly.subplots import make_subplots
 
-test_calibration_df = None
+
 calibration_period = None
 
 
@@ -101,8 +101,6 @@ def parse_contents(contents, filename, date):
 
             df = dash_utils.generate_basic_df(io.StringIO(decoded.decode("utf-8")))
             df = dash_utils.add_features_to_df(df)
-            global test_calibration_df
-            test_calibration_df = df.copy()
 
         else:
             return html.Div(["The uploaded filetype can only be CSV."])
@@ -131,8 +129,6 @@ def update_output(list_of_contents, list_of_names, list_of_dates):
 @app.callback(Output("main-tabs", "value"), Input("dataframe-json-storage", "data"))
 def init_tab_switch_on_upload(data):
 
-    # global test_calibration_df
-
     if data is None:
         raise PreventUpdate
 
@@ -146,8 +142,7 @@ def update_mhp_chart(json_data):
     if json_data is None:
         raise PreventUpdate
 
-    df = pd.DataFrame.from_dict(json_data[0])
-    df = df.set_index("timestamp")
+    df = dash_utils.load_df_from_local_storage(json_data)
     fig = charts.generate_subplots_chart(df)
 
     return fig
@@ -159,37 +154,34 @@ def update_rangeselector_chart(json_data):
     if json_data is None:
         raise PreventUpdate
 
-    df = pd.DataFrame.from_dict(json_data[0])
-    df = df.set_index("timestamp")
+    df = dash_utils.load_df_from_local_storage(json_data)
     fig = charts.generate_chart_with_rangeselector(df, feature_name="mhp")
     return fig
 
 
 @app.callback(
     Output("output-container-range-slider", "children"),
-    [Input("fig_with_rangeselector", "relayoutData")],
+    [Input("fig_with_rangeselector", "relayoutData"), Input("dataframe-json-storage", "data")],
 )
-def update_slider_output_values(relayoutData):
+def update_slider_output_values(relayoutData, json_data):
 
     if relayoutData is None:
         raise PreventUpdate
 
-    if test_calibration_df is None:
-        return html.Div(children=None)
+    if json_data is None:
+        raise PreventUpdate
 
+    df = dash_utils.load_df_from_local_storage(json_data)
     global calibration_period
 
-    calibration_period = slice(test_calibration_df.index[0], test_calibration_df.index[-1])
+    calibration_period = slice(df.index[0], df.index[-1])
 
     # Handles case when user zooms on chart to select range:
     if "xaxis.range[0]" in relayoutData:
         new_range_data_start = relayoutData["xaxis.range[0]"]
-        new_range_data_start = pd.to_datetime(new_range_data_start)
-
         new_range_data_stop = relayoutData["xaxis.range[1]"]
-        new_range_data_stop = pd.to_datetime(new_range_data_stop)
 
-        calibration_period = slice(new_range_data_start, new_range_data_stop)
+        calibration_period = slice(pd.to_datetime(new_range_data_start), pd.to_datetime(new_range_data_stop))
 
     # Handles case when user zooms on rangeslider to select range:
     elif "xaxis.range" in relayoutData:
@@ -199,7 +191,7 @@ def update_slider_output_values(relayoutData):
     else:
         new_range_data = calibration_period
 
-    diff = calibration_period.stop - calibration_period.start
+    diff = pd.to_datetime(calibration_period.stop) - pd.to_datetime(calibration_period.start)
 
     message_header = html.P("Selected calibration period:")
     message_list = html.Ul(
@@ -213,20 +205,23 @@ def update_slider_output_values(relayoutData):
 
 @app.callback(
     Output(component_id="mhpdt-results-div", component_property="children"),
-    [Input(component_id="button-mhpdt-calibration", component_property="n_clicks")],
+    [Input(component_id="button-mhpdt-calibration", component_property="n_clicks"), State("dataframe-json-storage", "data")],
 )
-def click_button_call_mhpdt_calibration(n_clicks):
+def click_button_call_mhpdt_calibration(n_clicks, json_data):
 
     if n_clicks is None:
         raise PreventUpdate
 
-    global test_calibration_df, calibration_period
+    global calibration_period
 
-    acceleration_data = test_calibration_df.copy().loc[calibration_period].round(3)
+    df = dash_utils.load_df_from_local_storage(json_data)
+
+    acceleration_data = df.copy().loc[calibration_period].round(3)
     calibration_json = dash_utils.accelerations_csv_to_json(acceleration_data, json_attribute="downTimeCalibrationData", file_path=None)
 
     r = requests.post(
-        "https://haris-oee-ml-azure-functions-staging.azurewebsites.net/api/MHPDT_cross_validation",
+        # "https://haris-oee-ml-azure-functions-staging.azurewebsites.net/api/MHPDT_cross_validation",
+        "http://localhost:7071/api/MHPDT_cross_validation",
         headers={"Content-Type": "application/json"},
         json=calibration_json,
     )
@@ -253,26 +248,29 @@ def store_mhpdt_calibration_parameters(input_data):
 
 @app.callback(
     Output(component_id="dt-calibration-graph", component_property="figure"),
-    Input(component_id="mhpdt-calibration-param-storage", component_property="data"),
+    [Input(component_id="mhpdt-calibration-param-storage", component_property="data"), State("dataframe-json-storage", "data")],
 )
-def plot_mhpdt_calibration_results(calibration_params):
+def plot_mhpdt_calibration_results(calibration_params, json_data):
 
     if calibration_params is None:
         raise PreventUpdate
 
-    global test_calibration_df, calibration_period
+    global calibration_period
 
-    df_calibration = test_calibration_df.copy().loc[calibration_period].round(3)
+    df = dash_utils.load_df_from_local_storage(json_data)
+    df_calibration = df.copy().loc[calibration_period].round(3)
 
-    # convert calibration params:
-    params_json_format = calibration_params["children"].replace("'", '"')
-    params = json.loads(params_json_format)
+    if 'calibration_score' in calibration_params["children"]:
+        # generate figure
+        # convert calibration params:
+        params_json_format = calibration_params["children"].replace("'", '"')
+        params = json.loads(params_json_format)
 
-    # generate figure
-    fig = charts.generate_mhpdt_calibration_chart(df_calibration, params)
+        fig = charts.generate_mhpdt_calibration_chart(df_calibration, params)
 
-    return fig
-
+        return fig
+    else:
+        return go.Figure()
 
 if __name__ == "__main__":
     app.run_server(debug=True)
--- dash_utils.py
+++ dash_utils.py
@@ -4,6 +4,7 @@ from typing import List, Dict, Union
 from sklearn.decomposition import PCA
 import json
 import logging
+
 # change default plotly theme
 import plotly.io
 
@@ -12,7 +13,8 @@ plotly.io.templates.default = "plotly_white"
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-def magnitude_highpass(df: pd.DataFrame, axes: List = ['x', 'y', 'z'], window_size: str = '3s') -> np.array:
+
+def magnitude_highpass(df: pd.DataFrame, axes: List = ["x", "y", "z"], window_size: str = "3s") -> np.array:
     """
 
     Parameters
@@ -72,8 +74,8 @@ def gravity_free_magnitude(df: pd.DataFrame, alpha: float = 0.8):
 
     """
 
-    df_gravity_free = df[['x', 'y', 'z']].copy()
-    for axis in ['x', 'y', 'z']:
+    df_gravity_free = df[["x", "y", "z"]].copy()
+    for axis in ["x", "y", "z"]:
         df_gravity_free[axis] = linear_acceleration(df_gravity_free[axis].copy(), alpha=alpha)
     magnitude = np.sqrt(df_gravity_free.pow(2).sum(axis=1))
 
@@ -91,12 +93,12 @@ def generate_basic_df(filepath: str) -> pd.DataFrame:
     -------
 
     """
-    df = pd.read_csv(filepath, sep=',')
-    df['timestamp'] = pd.to_datetime(df.timestamp, format='%Y-%m-%dT%H:%M:%S.%f')
-    df = df.set_index('timestamp')
+    df = pd.read_csv(filepath, sep=",")
+    df["timestamp"] = pd.to_datetime(df.timestamp, format="%Y-%m-%dT%H:%M:%S.%f")
+    df = df.set_index("timestamp")
 
     if not df.index.is_monotonic_increasing:
-        print('Warning: Dataframe DatetimeIndex is not monotonically increasing.')
+        print("Warning: Dataframe DatetimeIndex is not monotonically increasing.")
 
     return df
 
@@ -113,42 +115,32 @@ def add_features_to_df(df: pd.DataFrame, **kwargs) -> pd.DataFrame:
 
     """
 
-    alpha = kwargs.get('alpha', 0.8)
-    mhp_window_size = kwargs.get('mhp_window_size', '3s')
+    alpha = kwargs.get("alpha", 0.8)
+    mhp_window_size = kwargs.get("mhp_window_size", "3s")
     logger.setLevel(logging.INFO)
-    logger.info(f'using parameters mhp_window_size: {mhp_window_size} and alpha: {alpha}')
+    logger.info(f"using parameters mhp_window_size: {mhp_window_size} and alpha: {alpha}")
 
-    df['magnitude'] = np.sqrt(df.x ** 2 + df.y ** 2 + df.z ** 2)
-    df['mhp'] = magnitude_highpass(df, window_size=mhp_window_size)
-    df['no_gravity'] = gravity_free_magnitude(df, alpha=alpha)
-    df['pca'] = PCA(n_components=1).fit_transform(df[['x', 'y', 'z']].fillna(0))
+    df["magnitude"] = np.sqrt(df.x ** 2 + df.y ** 2 + df.z ** 2)
+    df["mhp"] = magnitude_highpass(df, window_size=mhp_window_size)
+    df["no_gravity"] = gravity_free_magnitude(df, alpha=alpha)
+    df["pca"] = PCA(n_components=1).fit_transform(df[["x", "y", "z"]].fillna(0))
 
     return df
 
 
-def df_to_json_accelerations(df: pd.DataFrame, timestamp_as_string=False) -> \
-        List[Dict[str,
-                  Union[str,
-                        Dict[str, float]]]]:
+def df_to_json_accelerations(df: pd.DataFrame, timestamp_as_string=False) -> List[Dict[str, Union[str, Dict[str, float]]]]:
     """
-        reproduces input identical to what GM consumes. useful for testing haris-oee-ml module prediction methods
+    reproduces input identical to what GM consumes. useful for testing haris-oee-ml module prediction methods
     """
     accelerations = []
 
-    for row in df[['x', 'y', 'z']].iterrows():
+    for row in df[["x", "y", "z"]].iterrows():
         if timestamp_as_string:
-            timestamp = row[0].strftime('%Y-%m-%dT%H:%M:%S.%f')
+            timestamp = row[0].strftime("%Y-%m-%dT%H:%M:%S.%f")
         else:
             timestamp = row[0]
 
-        temp_dict = {'timestamp': timestamp,
-                     'acceleration':
-                         {
-                             'x': row[1][0],
-                             'y': row[1][1],
-                             'z': row[1][2]
-                         }
-                     }
+        temp_dict = {"timestamp": timestamp, "acceleration": {"x": row[1][0], "y": row[1][1], "z": row[1][2]}}
         accelerations += [temp_dict]
     return accelerations
 
@@ -171,10 +163,7 @@ def andon_state_list_generator(states_array: pd.Series) -> List:
     return state_list
 
 
-def accelerometer_denoising(df: pd.DataFrame,
-                            cols=('x', 'y', 'z'),
-                            win_size=3,
-                            denoising_method='mean') -> pd.DataFrame:
+def accelerometer_denoising(df: pd.DataFrame, cols=("x", "y", "z"), win_size=3, denoising_method="mean") -> pd.DataFrame:
     """
 
     Parameters
@@ -192,10 +181,10 @@ def accelerometer_denoising(df: pd.DataFrame,
     df_denoised = df.copy()
 
     for col in cols:
-        col_name = f'{col}'
-        if 'median' == denoising_method:
+        col_name = f"{col}"
+        if "median" == denoising_method:
             df_denoised[col_name] = df_denoised[col].rolling(window=win_size).median().values
-        elif 'mean' == denoising_method:
+        elif "mean" == denoising_method:
             df_denoised[col_name] = df_denoised[col].rolling(window=win_size).mean().values
         else:
             raise AttributeError("Invalid ct denoising filter type. 'denoising_method' = 'median' or 'mean'")
@@ -218,86 +207,90 @@ def load_json_mobile_calibration_data(calibration_file_path: str) -> pd.DataFram
 
     """
 
-    with open(calibration_file_path, 'r') as json_file:
+    with open(calibration_file_path, "r") as json_file:
         survey_settings = json.loads(json_file.read())
 
     # access calibration dataframe:
-    calibration_data = survey_settings['surveyRun']['surveyRunDetail']
-    calibration_data = calibration_data['machineCalibrationData']['machineCalibrationDataLogs']
+    calibration_data = survey_settings["surveyRun"]["surveyRunDetail"]
+    calibration_data = calibration_data["machineCalibrationData"]["machineCalibrationDataLogs"]
 
     # iterate over beacon log entries:
     calibration_data_list = []
     for data in calibration_data:
-        array_to_append = [data['beaconLog']['mappedAdvertising']['time']]
-        array_to_append += [data['beaconLog']['mappedAdvertising']['accelerometerX']]
-        array_to_append += [data['beaconLog']['mappedAdvertising']['accelerometerY']]
-        array_to_append += [data['beaconLog']['mappedAdvertising']['accelerometerZ']]
-        array_to_append += [data['state']]
+        array_to_append = [data["beaconLog"]["mappedAdvertising"]["time"]]
+        array_to_append += [data["beaconLog"]["mappedAdvertising"]["accelerometerX"]]
+        array_to_append += [data["beaconLog"]["mappedAdvertising"]["accelerometerY"]]
+        array_to_append += [data["beaconLog"]["mappedAdvertising"]["accelerometerZ"]]
+        array_to_append += [data["state"]]
 
         calibration_data_list += [array_to_append]
 
     # create dataframe:
-    column_names = ['timestamp', 'x', 'y', 'z', 'state']
+    column_names = ["timestamp", "x", "y", "z", "state"]
     df = pd.DataFrame(calibration_data_list, columns=column_names)
-    df['timestamp'] = pd.to_datetime(df.timestamp, format='%Y-%m-%dT%H:%M:%S.%f')
-    df = df.set_index(keys='timestamp')
+    df["timestamp"] = pd.to_datetime(df.timestamp, format="%Y-%m-%dT%H:%M:%S.%f")
+    df = df.set_index(keys="timestamp")
 
     return df
 
 
 def str_date_converter(row):
     date_str = str(row).split()
-    return date_str[0] + 'T'+date_str[1][:-3]
+    return date_str[0] + "T" + date_str[1][:-3]
 
 
 def virtual_gateway_dummy_data(df_input, out_filepath):
     df = df_input.copy()
-    df['timestamp'] = df.index.to_series().apply(str_date_converter)
-    df = df.set_index('timestamp')
-    df.to_csv(out_filepath, sep=',', header=True, index=True)
+    df["timestamp"] = df.index.to_series().apply(str_date_converter)
+    df = df.set_index("timestamp")
+    df.to_csv(out_filepath, sep=",", header=True, index=True)
 
 
 def downtime_calibration_to_df(calibration_data_json):
     df_temp = pd.DataFrame(calibration_data_json)
     # df['ts'] = df.ts.str.split(':').apply(lambda x: x[:-1]).str.join('')
-    df_temp['timestamp'] = pd.to_datetime(df_temp.timestamp, format='%Y-%m-%dT%H:%M:%S.%f')
+    df_temp["timestamp"] = pd.to_datetime(df_temp.timestamp, format="%Y-%m-%dT%H:%M:%S.%f")
 
     df = pd.DataFrame(list(df_temp.acceleration.values), index=df_temp.timestamp)
-    df['state'] = df_temp.state.values
+    df["state"] = df_temp.state.values
 
     return df
 
 
-def rolling_std_feature(df, window_size='3s'):
+def rolling_std_feature(df, window_size="3s"):
 
-    rolling_std = df[['x', 'y', 'z']].rolling(window_size).std(ddof=1).fillna(0)
+    rolling_std = df[["x", "y", "z"]].rolling(window_size).std(ddof=1).fillna(0)
     # pca_feature = PCA(n_components=1).fit_transform(rolling_std)
 
     feature = np.sum(np.power(rolling_std, 3), axis=1)
 
     return feature
 
-def accelerations_csv_to_json(df: pd.DataFrame, json_attribute='cycleTimeCalibrationData', file_path=None):
+
+def accelerations_csv_to_json(df: pd.DataFrame, json_attribute="cycleTimeCalibrationData", file_path=None):
 
     calibration_json = {json_attribute: []}
-    for row in df[['x', 'y', 'z']].iterrows():
+    for row in df[["x", "y", "z"]].iterrows():
         try:
-            ts = row[0].strftime('%Y-%m-%dT%H:%M:%S.%f')
+            ts = row[0].strftime("%Y-%m-%dT%H:%M:%S.%f")
         except ValueError:
-            ts = row[0].strftime('%Y-%m-%dT%H:%M:%S.0')
+            ts = row[0].strftime("%Y-%m-%dT%H:%M:%S.0")
         x, y, z = row[1][0], row[1][1], row[1][2]
 
-        json_element = {'timestamp': ts,
-                        'acceleration': {
-                            'x': x,
-                            'y': y,
-                            'z': z
-                        }
-                        }
+        json_element = {"timestamp": ts, "acceleration": {"x": x, "y": y, "z": z}}
         calibration_json[json_attribute].append(json_element)
 
     if file_path:
-        with open(file_path, 'w') as f:
-            json.dump(calibration_json, f, sort_keys=True, indent=2, separators=(',', ': '))
+        with open(file_path, "w") as f:
+            json.dump(calibration_json, f, sort_keys=True, indent=2, separators=(",", ": "))
     else:
         return calibration_json
+
+
+def load_df_from_local_storage(json_data):
+
+    df = pd.DataFrame.from_dict(json_data[0])
+    df["timestamp"] = pd.to_datetime(df.timestamp)
+    df = df.set_index("timestamp")
+
+    return df
