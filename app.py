81f1cd5d3d681c799ddb3abea8a45972156dd32f
benmp2 <benmp2@github.com>
2021-05-16 09:55:47 +0200

    Adding TODO

--- app.py
+++ app.py
@@ -178,14 +178,32 @@ def update_mhp_chart(json_data):
     return fig
 
 
-@app.callback(Output("fig_with_rangeselector", "figure"), Input("dataframe-json-storage", "data"))
-def update_rangeselector_chart(json_data):
+@app.callback(
+    [Output(component_id="fig_with_rangeselector", component_property="figure")],
+    [
+        Input(component_id="dataframe-json-storage", component_property="data"),
+        Input(component_id="calibration-date-filter-button", component_property="n_clicks"),
+        State(component_id="calibration-filter-start-date", component_property="value"),
+        State(component_id="calibration-filter-end-date", component_property="value"),
+    ],
+)
+def update_rangeselector_chart(json_data, n_clicks):
 
     if json_data is None:
         raise PreventUpdate
 
     df = dash_utils.load_df_from_local_storage(json_data)
+
+    if n_clicks is not None:
+        # TODO:
+        # - make sure input dates are in the correct format
+        # - date filtering is with in df index range
+        # - if all clear then filter figure?
+        # - make sure relayoutData is triggered?
+        pass
+
     fig = charts.generate_chart_with_rangeselector(df, feature_name="mhp")
+
     return fig
 
 
