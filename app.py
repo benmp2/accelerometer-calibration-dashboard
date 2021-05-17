2a59227d0744f7aec492fd5805c563e564bf4e90
benmp2 <benjamin.powell@harisdigital.com>
2021-05-17 17:49:49 +0200

    black formatter

--- app.py
+++ app.py
@@ -269,7 +269,7 @@ def click_button_call_mhpdt_calibration(n_clicks, json_data, calibration_period_
     acceleration_data = df.copy().loc[calibration_period].round(3)
     calibration_json = dash_utils.accelerations_csv_to_json(acceleration_data, json_attribute="downTimeCalibrationData", file_path=None)
 
-    azure_func_url = os.environ.get("AZURE_FUNC_URL","http://localhost:7071")
+    azure_func_url = os.environ.get("AZURE_FUNC_URL", "http://localhost:7071")
     r = requests.post(
         f"{azure_func_url}/api/MHPDT_cross_validation",
         headers={"Content-Type": "application/json"},
